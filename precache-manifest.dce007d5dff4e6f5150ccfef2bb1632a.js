76e465b66a6e451dce3f39df7f1beb28ff56d53c
Stephen Renda <stephenrendajr@gmail.com>
2020-05-25 23:28:25 -0700

    Updates

---
+++ precache-manifest.dce007d5dff4e6f5150ccfef2bb1632a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99aed4942a0e6771961fd7694007c054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a17bb99b8083ffe362f",
+    "url": "/static/css/main.20fd1640.chunk.css"
+  },
+  {
+    "revision": "1a21e92e24477c464780",
+    "url": "/static/js/2.eb54353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb54353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a17bb99b8083ffe362f",
+    "url": "/static/js/main.06012304.chunk.js"
+  },
+  {
+    "revision": "2adf10464682cab25afe",
+    "url": "/static/js/runtime-main.c0ef1627.js"
+  }
+]);
