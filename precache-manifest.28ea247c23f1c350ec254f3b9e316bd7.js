991e6234801589241d9449067d2b451fee399c4e
Stephen Renda <stephenrendajr@gmail.com>
2020-05-26 00:04:12 -0700

    Updates

---
+++ precache-manifest.28ea247c23f1c350ec254f3b9e316bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4791eaf63ee6c7ebd365b1c795f0d6",
+    "url": "/Tic-Tac_Toe/index.html"
+  },
+  {
+    "revision": "4a17bb99b8083ffe362f",
+    "url": "/Tic-Tac_Toe/static/css/main.20fd1640.chunk.css"
+  },
+  {
+    "revision": "1a21e92e24477c464780",
+    "url": "/Tic-Tac_Toe/static/js/2.eb54353c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac_Toe/static/js/2.eb54353c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a17bb99b8083ffe362f",
+    "url": "/Tic-Tac_Toe/static/js/main.06012304.chunk.js"
+  },
+  {
+    "revision": "3102b1aea0d00f87b858",
+    "url": "/Tic-Tac_Toe/static/js/runtime-main.dbcc87f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dce007d5dff4e6f5150ccfef2bb1632a.js"
+  "/Tic-Tac_Toe/precache-manifest.28ea247c23f1c350ec254f3b9e316bd7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tic-Tac_Toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
